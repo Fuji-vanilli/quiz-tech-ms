af6c2e1f6ace647ef1abcb898685b10d25623a54
Fuji vanilli <ijufillivan@gmail.com>
2024-01-31 12:50:06 +0300

    first commit

---
+++ back/category-service/src/test/java/com/quiztech/categoryservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiztech.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back/question-service/src/test/java/com/quiztech/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiztech.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
