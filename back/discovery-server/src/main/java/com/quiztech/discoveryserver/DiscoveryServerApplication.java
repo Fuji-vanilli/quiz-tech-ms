29fb34da89fcd8b441f0f5ab8ae1138ae8fc2d02
fuji-vanilli <ijufillivan@gmail.com>
2024-02-23 06:44:33 +0300

    update

--- back/discovery-server/src/main/java/com/quiztech/discoveryserver/DiscoveryServerApplication.java
+++ back/discovery-server/src/main/java/com/quiztech/discoveryserver/DiscoveryServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.io.InputStream;
+import java.util.stream.IntStream;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
