8244c7b1448bf5ffd5b40ea75a09354c91db778b
fuji-vanilli <ijufillivan@gmail.com>
2024-02-26 07:35:41 +0300

    update

--- back/gateway-service/src/main/java/com/quiztech/gatewayservice/GatewayServiceApplication.java
+++ back/gateway-service/src/main/java/com/quiztech/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,22 @@ package com.quiztech.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+	}
 }
--- back/gateway-service/src/main/java/com/quiztech/gatewayservice/security/SecurityConfig.java
+++ back/gateway-service/src/main/java/com/quiztech/gatewayservice/security/SecurityConfig.java
@@ -4,22 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
-
-    @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity
                 .authorizeExchange(auth-> auth.anyExchange().authenticated());
 
         httpSecurity
                 .cors(Customizer.withDefaults())
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
 
         return httpSecurity.build();
     }
+
+
 }
