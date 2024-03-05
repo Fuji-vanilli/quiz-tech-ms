9694fff6016933d8b2d859a6cf610283957d48b5
fuji-vanilli <ijufillivan@gmail.com>
2024-03-05 19:47:21 +0300

    entity user

---
+++ back/user-service/src/main/java/com/quiztech/userservice/entities/User.java
@@ -0,0 +1,30 @@
+package com.quiztech.userservice.entities;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+@Document(value = "user")
+public class User {
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String photo;
+    private List<String> competences= new ArrayList<>();
+    private List<String> organisations= new ArrayList<>();
+    private String biography;
+    private String description;
+    private BigDecimal followers;
+    private BigDecimal following;
+    private List<String> linkNetwork= new ArrayList<>();
+}
