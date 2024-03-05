91821bf24d3d3e3b1b23c0970a5deaedba827c95
fuji-vanilli <ijufillivan@gmail.com>
2024-03-05 17:36:55 +0300

    update

--- front-quiz/src/app/pages/user/profile-user/profile-user.component.ts
+++ front-quiz/src/app/pages/user/profile-user/profile-user.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { KeycloakProfile } from 'keycloak-js';
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexStroke, ApexTooltip, ApexXAxis, ChartComponent } from 'ng-apexcharts';
+import { ResultQuizService } from 'src/app/services/result-quiz.service';
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+};
 
 @Component({
   selector: 'app-profile-user',
@@ -12,9 +22,37 @@ export class ProfileUserComponent implements OnInit{
   
   profile!: KeycloakProfile;
 
-  constructor(private keycloakService: KeycloakService) {
-    
-  }
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  resultQuizSummary= new Map<any, any>();
+
+  constructor(private keycloakService: KeycloakService,
+              private resultQuiz: ResultQuizService) {
+    this.chartOptions = {
+      series: [],
+      
+      chart: {
+        height: 350,
+        type: "area"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth"
+      },
+      xaxis: {
+        categories: []
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm"
+        }
+      }
+    };
+}  
+  
   ngOnInit(): void {
     this.loadProfile();
   }
@@ -25,6 +63,38 @@ export class ProfileUserComponent implements OnInit{
         this.profile= profile
       }
     )
+  }
+
+  
+  loadResultSummary(email: any) {
+    this.resultQuiz.getResultSummary(email).subscribe({
+      next: response => {
+        this.resultQuizSummary = response.data;
+        console.log("result summary: ", this.resultQuizSummary);
+  
+        let series: any[] = [];
+        let categories: any[] = [];
+  
+        for (const [key, value] of Object.entries(this.resultQuizSummary)) {
+          series.push({
+            name: key,
+            data: value
+          });
+          categories.push(key);
+        }
+  
+        this.chartOptions.series = series;
+        if (this.chartOptions && this.chartOptions.xaxis) {
+          this.chartOptions.xaxis.categories = categories;
+        }
+  
+        this.chart.updateOptions(this.chartOptions);
+  
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
   }
 
   
