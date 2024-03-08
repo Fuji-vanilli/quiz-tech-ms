8462e670b6a0c12f282d2eb42262b5a85770593d
fuji-vanilli <ijufillivan@gmail.com>
2024-03-08 09:27:48 +0300

    update

--- front-quiz/src/app/pages/user/user-list/user-list.component.ts
+++ front-quiz/src/app/pages/user/user-list/user-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
 
+  users: User[]= [];
+
+  constructor(private userService: UserService ) {}
+
+  ngOnInit(): void {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.userService.fetchAll().subscribe({
+      next: response=> {
+        this.users= response.data.users
+        console.log('users: ', this.users);
+        
+      },
+      error: err=> {
+        console.log(err);
+        
+      }
+    })
+  }
 }
