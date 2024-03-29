1afa39b2ec4ddacc40d398a80c58cab5e8f4159c
fuji-vanilli <ijufillivan@gmail.com>
2024-03-29 10:19:04 +0300

    update

--- back/quiz-service/src/main/java/com/quiztech/quizservice/service/QuizServiceImpl.java
+++ back/quiz-service/src/main/java/com/quiztech/quizservice/service/QuizServiceImpl.java
@@ -141,6 +141,7 @@ public class QuizServiceImpl implements QuizService {
         quiz.setCategoryId(request.getCategoryId());
         quiz.setImageUrl(request.getImageUrl());
         quiz.setTitle(request.getTitle());
+        quiz.setLanguage(request.getLanguage());
         quiz.setDifficulty(request.getDifficulty());
         quiz.setDescription(request.getDescription());
         quiz.setLastUpdateDate(new Date());
