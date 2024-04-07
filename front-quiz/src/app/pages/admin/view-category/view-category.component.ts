fd50460f57749db6f207d95f54f91376451545a2
fuji-vanilli <ijufillivan@gmail.com>
2024-04-07 09:56:49 +0300

    update

--- front-quiz/src/app/pages/admin/view-category/view-category.component.ts
+++ front-quiz/src/app/pages/admin/view-category/view-category.component.ts
@@ -40,6 +40,7 @@ export class ViewCategoryComponent implements OnInit {
   ngOnInit(): void {
     this.loadCategories();
     this.categoryId= this.routeActive.snapshot.params['id'];
+    window.scrollTo({top: 0, behavior: 'smooth'})
   }
 
 
@@ -70,7 +71,7 @@ export class ViewCategoryComponent implements OnInit {
       cancelButtonColor: 'rgba(0, 0, 0, 0.3)',
       confirmButtonText: 'Yes, Delete',
       confirmButtonColor: '#bb2649',
-      background: 'rgba(255, 255, 255, 0.2)',
+      background: '#1E293B',
       color: '#fff'
     }).then(result=> {
        if (result.isConfirmed) {
